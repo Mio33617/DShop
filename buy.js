1922227d217538d9d34cdd0858de7b2dedb6731f
Mio6611 <thino66776611>
2025-08-20 11:33:46 +0630

    add2

---
+++ buy.js
@@ -0,0 +1,12 @@
+const element = document.getElementById('order');
+const myname = document.getElementById("myname");
+const add = document.getElementById("add");
+
+element.addEventListener("click",myfunction);
+function myfunction() {
+    if (myname.value === "" || add.value === "") {
+        alert("From is empty.");
+    } else {
+        window.location.href = "./index.html"; 
+    }
+}
